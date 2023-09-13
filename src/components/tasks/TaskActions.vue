09b8482d723797390eed031feade20a6781e44de
junior <antonioivo.3@gmail.com>
2023-09-13 16:22:47 -0300

    firts commit

---
+++ src/components/tasks/TaskActions.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="task-actions">
+            <button class="btn btn-sm btn-circle btn-outline-secondary me-1" @click="emit('edit')">
+                <iconPencil />
+
+            </button>
+            <button class="btn btn-sm btn-circle btn-outline-danger" @click="emit('remove')">
+               <iconTrash />
+            </button>
+        </div>
+</template>
+
+<script setup>
+import iconPencil from "../icons/iconPencil.vue";
+import iconTrash from "../icons/iconTrash.vue";
+
+const emit  = defineEmits(['edit','remove'])
+
+</script>
---
+++ src/components/tasks/Tasks.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card mt-2" v-if="tasks && show">
+        <ul class="list-group list-group-flush">
+            <Task v-for="task in tasks" :key="task.id" 
+            :task="task"
+             @updated="handleUpdatedTask" 
+             @completed="handleCompletedTask"
+             @removed="handleRemovedTask"
+
+             />
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import {useTaskStore} from "../../stores/task";
+import Task from './Task.vue'
+
+const store = useTaskStore();
+const{handleUpdatedTask,handleCompletedTask,handleRemovedTask} = store
+
+defineProps({
+    tasks:Array,
+    show:{
+        type:Boolean,
+        default:true,
+    }
+})
+</script>
